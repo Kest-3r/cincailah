96f247162a48e983f680a8f2f88c0fee8036c28b
Kest-3r <kest3r.05@gmail.com>
2025-08-30 00:25:48 +0800

    navigation and welcome page ui edits

--- lib/widgets/nav.dart
+++ lib/widgets/nav.dart
@@ -10,13 +10,27 @@ class Nav extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey[200], // background color for the nav bar
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+      margin: const EdgeInsets.all(12), // space around the nav bar
+      padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 16),
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.white, // background of the nav bar
+        borderRadius: BorderRadius.circular(40), // 👈 curved edges
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 8,
+            offset: const Offset(2, 2),
+          ),
+        ],
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           IconButton(
-            icon: Image.asset("images/Home.png", width: 80, height: 80),
+            padding: EdgeInsets.zero, // 👈 removes extra space
+            constraints: const BoxConstraints(), // 👈 removes default min size
+            icon: Image.asset("images/Home.png", width: 60, height: 60),
             onPressed: () {
               Navigator.push(
                 context,
@@ -25,7 +39,9 @@ class Nav extends StatelessWidget {
             },
           ),
           IconButton(
-            icon: Image.asset("images/Diary.png", width: 80, height: 80),
+            padding: EdgeInsets.zero, // 👈 removes extra space
+            constraints: const BoxConstraints(), // 👈 removes default min size
+            icon: Image.asset("images/Diary.png", width: 60, height: 60),
             onPressed: () {
               Navigator.push(
                 context,
@@ -34,7 +50,9 @@ class Nav extends StatelessWidget {
             },
           ),
           IconButton(
-            icon: Image.asset("images/Relax.png", width: 80, height: 80),
+            padding: EdgeInsets.zero, // 👈 removes extra space
+            constraints: const BoxConstraints(), // 👈 removes default min size
+            icon: Image.asset("images/Relax.png", width: 60, height: 60),
             onPressed: () {
               Navigator.push(
                 context,
@@ -43,7 +61,9 @@ class Nav extends StatelessWidget {
             },
           ),
           IconButton(
-            icon: Image.asset("images/Profile.png", width: 80, height: 80),
+            padding: EdgeInsets.zero, // 👈 removes extra space
+            constraints: const BoxConstraints(), // 👈 removes default min size
+            icon: Image.asset("images/Profile.png", width: 60, height: 60),
             onPressed: () {
               Navigator.push(
                 context,
